950b542097e9990663835f773f86cbf4093c1b0a
krabobmkd <victorien.ferry@free.fr>
2024-12-03 17:48:20 +0100

    woot md2ag

--- buildtools/md2ag.cpp
+++ buildtools/md2ag.cpp
@@ -238,6 +238,43 @@ string replaceLinks(const string s)
     final += s.substr(iprev,(i== string::npos)?i:(i-iprev));
     return final;
 }
+//  ![Game driver list interface](guideimg/mmcontrols)
+string replaceImg(const string s)
+{
+
+    bool inpa=false,inpb=false;
+    size_t l = s.length() ;
+    size_t i=0,iprev=0;
+//    string final;
+    stringstream ssfinal;
+    while(i<l)
+    {
+       i = s.find("![",iprev);
+       if(i== string::npos) break;
+       size_t ib = s.find("]",i+2);
+        if(ib== string::npos) break;
+
+        size_t ic = s.find("(",ib+1);
+        if(ic== string::npos) break;
+        size_t id = s.find(")",ic+1);
+        if(id== string::npos) break;
+
+         string visible = s.substr(i+2,ib-(i+2));
+         string link = s.substr(ic+1,id-(ic+1));
+         link = rreplace(link,"#","");
+
+        ssfinal << s.substr(iprev,(i== string::npos)?i:(i-iprev));
+// @{"Picture of a Workbench Screen" SYSTEM sys:utilities/Display sys:Workbench.pic}
+        ssfinal << "@{\" " << visible << " \" SYSTEM multiview "<<link<<".gif "<< "}" ;
+
+        iprev = id+1;
+        i=iprev;
+    }
+    ssfinal << s.substr(iprev,(i== string::npos)?i:(i-iprev));
+    return ssfinal.str();
+}
+
+
 string replaceBolds(const string s)
 {
     stringstream ss;
@@ -276,8 +313,8 @@ void Agdoc::export_ag(ofstream &ofs)
 
          ofs << "@NODE "<<nodename<<" \""<< p->_tit <<"\"\n";
 
-         ofs << "@WORDWRAP\n@Width 72\n@TAB 3\n";
-          //  ofs << "@SMARTWRAP\n@TAB 3\n";
+//         ofs << "@WORDWRAP\n@Width 74\n@TAB 2\n";
+            ofs << "@SMARTWRAP\n@TAB 3\n";
 
 //@SMARTWRAP
 //@TAB 3
@@ -295,6 +332,133 @@ void Agdoc::export_ag(ofstream &ofs)
 
 
 @TOC Glossary
+
+
+Attribute Commands
+******************
+
+Attribute Commands
+==================
+
+@{b}{<label> <command>}@{ub}
+     Specify a hypertext link.  Not really an attribute per se, but shares a
+     similar syntax and scope.  It may be specified anywhere on a line.
+
+     The command is usualy "LINK", to specify a normal hypertext link.
+     There are other commands as follows:
+
+    @{b}ALINK <path/name> <line>@{ub}
+          Display the hypertext node in a new window, starting at <line>.
+          This no longer works in V39 and up (why not??).
+
+    @{b}CLOSE@{ub}
+          Close the window, to be used with ALINK windows.  Doesn't work in
+          V39 and up.
+
+    @{b}LINK <path/name> <line>@{ub}
+          Display the hypertext node, starting at <line>.
+
+    @{b}RX <command>@{ub}
+          Execute an ARexx script.
+
+    @{b}RXS <command>@{ub}
+          Execute an ARexx string file.
+
+    @{b}SYSTEM <command>@{ub}
+          Execute an AmigaDOS command.
+
+    @{b}QUIT@{ub}
+          Shutdown the current database.  Doesn't work in V39 and up.
+
+     The node is the name of a node in this document, or a path to a node in
+     another document.  In AmigaOS 3.0 and above, the node may be a path to
+     any type of file which is recognized by the Datatypes system.
+
+@{b}{AMIGAGUIDE}@{ub}
+     Displays the word "Amigaguide(R)" in bold.  V40.
+
+@{b}{APEN <n>}@{ub}
+     Change the foreground (text) colour to specified pen number.  V40.
+
+@{b}{B}@{ub}
+     Turn on bolding.  V39.
+
+@{b}{BG <colour>}@{ub}
+     Change the background to a preferences defined colour.  Colour can be:
+
+    @{b}Text@{ub}
+    @{b}Shine@{ub}
+    @{b}Shadow@{ub}
+    @{b}Fill@{ub}
+    @{b}FillText@{ub}
+    @{b}Background@{ub}
+    @{b}Highlight@{ub}
+@{b}{BODY}@{ub}
+     Restore default formatting for normal body text.  V40.
+
+@{b}{BPEN <n>}@{ub}
+     Change the background colour to specified pen number.  V40.
+
+@{b}{CLEARTABS}@{ub}
+     Restore default tabs.  V40.
+
+@{b}{CODE}@{ub}
+     Turn off wordwrapping.  V40.
+
+@{b}{FG <colour>}@{ub}
+     Change the foreground colour.  See command BG for colours.  V40.
+
+@{b}{I}@{ub}
+     Turn on italics.  V39.
+
+@{b}{JCENTER}@{ub}
+     Turn on centered justification.  V40.
+
+@{b}{JLEFT}@{ub}
+     Turn on left justification.  V40.
+
+@{b}{JRIGHT}@{ub}
+     Turn on right justification.  V40.
+
+@{b}{LINDENT <n>}@{ub}
+     Specify an indent in spaces for the body of paragraphs.  V40.
+
+@{b}{LINE}@{ub}
+     Force a line feed without starting a new paragraph.  V40.
+
+@{b}{PAR}@{ub}
+     Specifies the end of the paragraph, equivalent to two line feeds and
+     usable with SMARTWRAP.  V40.
+
+@{b}{PARD}@{ub}
+     Reset to default paragraph settings:  APEN to 1, BPEN to 0, original
+     font, and LINDENT to 0.  V40.
+
+@{b}{PARI <n>}@{ub}
+     Specify an indent in spaces for the first line of a paragraph.  Value
+     is relative to LINDENT and may be negative.  V40.
+
+@{b}{PLAIN}@{ub}
+     Turns off all style attributes (bold, italics, underlineing).  V40.
+
+@{b}{SETTABS <n> ... <n>}@{ub}
+     Specify a series of tab stops in spaces.  V40.
+
+@{b}{TAB}@{ub}
+     Outputs a real tab character.  V40.
+
+@{b}{U}@{ub}
+     Turn on underlining.  V39.
+
+@{b}{UB}@{ub}
+     Turn off bolding.  V39.
+
+@{b}{UI}@{ub}
+     Turn off italics.  V39.
+
+@{b}{UU}@{ub}
+     Turn off underlining.  V39.
+https://www.lysator.liu.se/amiga/code/guide/amigaguide.guide
 */
          // now need to write  title name in text also
          ofs << "\n  @{b}" << p->_tit << "@{ub}\n\n" ;
@@ -302,7 +466,7 @@ void Agdoc::export_ag(ofstream &ofs)
         nodeison = true;
     }
     // treat body
-
+    bool firstdot=false;
     for(string s : p->_t)
     {
         if(s.length() ==0) { ofs << "\n";
@@ -333,12 +497,21 @@ void Agdoc::export_ag(ofstream &ofs)
             char dotch = (char)149;
         //    if(startWith(ttrim,"* ") ) otherindex +=
              if(startWith(ttrim,"- "))  {
+              if(firstdot) ofs << "@LINDENT "<< (sindent.length()+2);
                 otherindex += "   ";
                 dotch = '-';
+             }else
+             {
+                if(firstdot) ofs << "@LINDENT "<< (sindent.length());
+
              }
             // 149 = dot in ansi
             ofs << otherindex << dotch << ttrim.substr(1) << "\n";
+
+            firstdot = false;
             continue;
+        } else {
+        firstdot = true;
         }
        // [Prerequesite and installation](#prerequesite-and-installation])
        // replace links
