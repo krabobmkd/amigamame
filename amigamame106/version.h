13881445cec110d29737398553fd7bf2e444a657
krabobmkd <victorien.ferry@free.fr>
2025-10-22 11:24:10 +0200

    notes

--- amigamame106/version.h
+++ amigamame106/version.h
@@ -4,8 +4,9 @@
 
 #include "mame_ver.h"
 
-#define APPNAMEA  "Mame 0.106 MiniMix 2025 1.7beta"
+#define APPNAMEA  "Mame 0.106 MiniMix 2025 1.7"
 
-#define APPVERNUM  "a1.7beta"
+#define APPVERNUM  "a1.7"
 
 #endif
+
